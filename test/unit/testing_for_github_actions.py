5623325c7d59933d53dc0aa544ef1e0fa2d695e9
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-10-20 12:03:49 -0600

    added one test for error handling

--- test/unit/testing_for_github_actions.py
+++ test/unit/testing_for_github_actions.py
@@ -1,5 +1,7 @@
 import sys
 import unittest
+from unittest import mock
+import mrcfile
 
 sys.path.append('src/')  # noqa
 
@@ -10,9 +12,21 @@ import utils
 # This unit testing WILL be executed by GitHub actions
 class TestDataExtractor(unittest.TestCase):
 
-    # This is just a dummy test for now, but we'll make real tests
-    def test_DummyTest(self):
+    def test_FileNotFound_error(self):
 
+        self.assertEqual(1, 1)  # dummy test I'll delete soon
+
+        # simulate a parsing error for enzyme_code = 1
+        with self.assertRaises(SystemExit) as cm:
+            utils.pull_micrographs(1)
+        self.assertEqual(cm.exception.code, 2)
+
+
+class TestParticlePicker(unittest.TestCase):
+
+    def test_(self):
+
+        # no clue how to test this if the data is outside out repo
         self.assertEqual(1, 1)
 
 
