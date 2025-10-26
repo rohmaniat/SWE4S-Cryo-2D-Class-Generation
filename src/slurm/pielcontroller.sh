16f4a8e4dd590af2dd92aa66bd363f82e5eb99bf
Rohan Tonk <rohan.tonk@colorado.edu>
2025-10-26 17:49:14 -0600

    log files now get placed in their own folder

--- src/slurm/pielcontroller.sh
+++ src/slurm/pielcontroller.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+#SBATCH --partition=titan
 #SBATCH --job-name=download_data
-#SBATCH --output=download_data.log
-#SBATCH --time=00:10:00
-#SBATCH --ntasks=1
-#SBATCH --mem=1G
+#SBATCH --output=Data/log_files/download_data_%j.log
+#SBATCH --time=06:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=roto9457@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=6
+#SBATCH --mem=32G
+##SBATCH --gres=gpu:1
 
-# Optional: load any required modules (depends on your cluster)
+# load any required modules
+module purge
 module load curl
+module list
 
 # Create a data directory in your scratch or working space
 mkdir -p $SLURM_SUBMIT_DIR/Data
@@ -15,12 +22,12 @@ mkdir -p $SLURM_SUBMIT_DIR/Data
 cd $SLURM_SUBMIT_DIR/Data
 
 # Download the file with curl
-curl -L -O https://calla.rnet.missouri.edu/cryoppp/10061.tar.gz
+curl -L -O https://calla.rnet.missouri.edu/cryoppp/10005.tar.gz
 
 # Download all files from the webpage
 # wget -r -np -nH --cut-dirs=1 -P data https://calla.rnet.missouri.edu/cryoppp/10061.tar.gz
 
 # Print confirmation
-echo "Download complete: $(ls -lh dataset.zip)"
+echo "Download complete: $(ls -lh)"
 
 # we may actually need to use a special transfer node to run this command 
