74208713427b6d87ae718d03c9a6d159edb9c7e9
Jared Roth <Jared.l.roth@gmail.com>
2025-11-08 17:42:01 -0700

    Add func testing

--- test/func/func_testing.sh
+++ test/func/func_testing.sh
@@ -3,3 +3,12 @@
 test -e ssshtest || curl -q -O https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+# ----- PREDICTION TESTING -----
+# First, just run a basic predict execution
+run standard_predict python src/predict.py \
+--mrc_file "test/func/EXAMPLE_Falcon_2012_06_12-14_33_35_0.mrc" \
+--output_csv "test/func/coord_predictions.csv" \
+--model_path "src/models/my_model.pt" \
+--threshold 1.5
+assert_in_stdout "Model loaded successfully!"
+assert_exit_code 0
