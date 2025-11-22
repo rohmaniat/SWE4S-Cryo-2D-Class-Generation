44ca05e5962ff1dc7c3bf9c941c0ad6ef98cd7a3
rohmaniat <84361227+rohmaniat@users.noreply.github.com>
2025-11-22 15:13:31 -0700

    new era for unit tests

---
+++ test/unit/utils_tests.py
@@ -0,0 +1,33 @@
+import sys
+import unittest
+from unittest import mock
+import torch
+
+sys.path.append('src/')  # noqa
+
+# import random
+import utils
+import predict
+
+
+# This unit testing WILL be executed by GitHub actions
+class TestDataExtractor(unittest.TestCase):
+
+    def test_FileNotFound_error(self):
+
+        # simulate a parsing error for enzyme_code = 1
+        with self.assertRaises(SystemExit) as cm:
+            utils.pull_micrographs(1)
+        self.assertEqual(cm.exception.code, 2)
+
+
+class TestParticlePicker(unittest.TestCase):
+
+    def test_(self):
+
+        # no clue how to test this if the data is outside out repo
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
